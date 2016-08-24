7a8530a3d89581f1f2052d667f7fd54b05e30495
MR027750 <mike.rodriguez@cerner.com>
2016-08-24 12:06:15 -0500

    - improve durability tests

--- src/test/clojure/clara/test_durability.clj
+++ src/test/clojure/clara/test_durability.clj
@@ -25,6 +25,62 @@
 (def rulebase-data (java.io.File/createTempFile "rulebase" ".dat"))
 (def session-data (java.io.File/createTempFile "session" ".dat"))
 
+(defn check-fact
+  "Helper for checking facts in durability-test."
+  [expected-fact fact]
+
+  ;; Test equality first.  The tests need to be stricter than this, but if this isn't true nothing
+  ;; else will be.  This will give the best failure messages first too in the more obvious non-equal
+  ;; cases.
+  (when (is (= expected-fact fact)
+            "The expected and actual must be equal")
+    
+    (or (identical? expected-fact fact)
+        (and (is (= (coll? expected-fact)
+                    (coll? fact))
+                 (str "not identical? with expected should mean both are collections" \newline
+                      "expected fact: " expected-fact \newline
+                      "fact: " fact \newline))
+
+             (is (= (count expected-fact)
+                    (count fact))
+                 (str "not identical? with expected should mean both are the"
+                      " same size collections" \newline
+                      "expected fact: " expected-fact \newline
+                      "fact: " fact \newline))
+
+             ;; Not handling collection types not being used right now in the test.
+             ;; This is only using the rules from clara.durability-rules.
+             ;; If more sophisticated aggregates are used in join bindings, fact bindings,
+             ;; or accumulated results in these tests case, the testing here will have to
+             ;; be made more robust to show it.
+             (cond
+               (sequential? expected-fact)
+               (and (is (sequential? fact)
+                        (str "expected is sequential?" \newline
+                             "expected fact: " expected-fact \newline
+                             "fact: " fact \newline))
+                    (mapv check-fact
+                          expected-fact
+                          fact))
+
+               (set? expected-fact)
+               (and (is (set? fact)
+                        "expected is set?")
+                    (every? #(is (identical? (expected-fact %) %)
+                                 (str "the fact from one set must be found in the"
+                                      " expected set and be identical? to it" \newline
+                                      "expected fact: " expected-fact \newline
+                                      "fact: " fact \newline))
+                            fact))
+
+               :else
+               (is false
+                   (str "Must find a matching comparison with the expected."
+                        "  Most of the time this means the facts should be identical?" \newline
+                        "expected fact: " expected-fact \newline
+                        "fact: " fact \newline)))))))
+
 (defn durability-test
   "Test runner to run different implementations of d/ISessionSerializer."
   [serde-type]
@@ -124,7 +180,51 @@
               restored-node-with-meta (-> restored-rulebase
                                           :id-to-node
                                           (get (:id node-with-meta)))]
-          (is (= (meta node-with-meta) (meta restored-node-with-meta))))))))
+          (is (= (meta node-with-meta) (meta restored-node-with-meta)))))
+
+      (testing (str "facts given to serialize-facts of IWorkingMemorySerializer"
+                    " from ISessionSerializer have identity relationships"
+                    " retained and accumulated values present.")
+        ;; Unfortunately what seems like the best way to test this right now is to just manually
+        ;; write out the whole expectation.  This is brittle, but hopefully doesn't change
+        ;; that often.
+        (let [cold20 (-> cold-res first :?c)
+              unpaired-ws10 (-> unpaired-res first :?ws)
+              temp-his (-> temp-his-res first :?his)
+              temps-under-thresh (-> temps-under-thresh-res first :?tut)
+              [hot30 hot40 hot50] (->> hot-res (map :?h) (sort-by :temperature))
+
+              ;; All of these facts must have an identical? relationship (same object references)
+              ;; as the actual facts being tested against.
+              expected-facts [temp50
+                              temp40
+                              temp30
+                              temp20
+                              [temp50 temp40 temp30 temp20]
+                              cold20
+                              unpaired-ws10
+                              temp-his
+                              ws50
+                              ws40
+                              ws10
+                              thresh50
+                              temps-under-thresh
+                              hot40
+                              hot30
+                              hot50
+                              [temp40 temp30 temp20]]]
+          
+          (is (= (count expected-facts)
+                 (count facts))
+              (str "expected facts:" \newline
+                   (vec expected-facts) \newline
+                   "actual facts:" \newline
+                   (vec facts)))
+          
+          (doseq [i (range (count expected-facts))
+                  :let [expected-fact (nth expected-facts i)
+                        fact (nth facts i)]]
+            (check-fact expected-fact fact)))))))
  
 (deftest test-durability-fressian-serde
   (durability-test :fressian))
